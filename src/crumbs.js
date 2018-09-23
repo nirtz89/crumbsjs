c08fdeb624b53c312ac5fd00007053b3a67d4743
Nir Tzezana <nirtz89@gmail.com>
2018-09-23 21:35:56 +0300

    Added the expires function

--- src/crumbs.js
+++ src/crumbs.js
@@ -1,12 +1,21 @@
-let crumbs = ()=> {
+let crumbs = function() {
         return {
-            set : function(cookie_name,value,expires) {
+            set : function(name,value,expires,domain) {
             try {
-                document.cookie =  `${cookie_name}=${value}`;
+                var cookie_expires = "",
+                    cookie_domain = "";
+                if (expires != undefined) {
+                    cookie_expires = new Date();
+                    cookie_expires = `expires=${cookie_expires.setTime(cookie_expires.getTime()+(expires*24*60*60*1000))};`;
+                }
+                if (domain != undefined) {
+                    
+                }
+                document.cookie =  `${name}=${value};${cookie_expires}`;
             }
             catch (e) {
                 console.log(e);
             }
         }
     }
-}
+}();
