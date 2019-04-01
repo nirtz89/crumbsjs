0bf0852921a0d82050cde7e4e51a05f71b07c1ca
Johann-S <johann.servoire@gmail.com>
2019-04-01 11:39:03 +0200

    add rollup and babel to bundle and convert to es5

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,37 @@
 const gulp = require('gulp'); 
 const minify = require('gulp-minify'); 
-const concat = require('gulp-concat'); 
+const concat = require('gulp-concat');
+const gulpRollup = require('gulp-rollup');
+const babel = require('rollup-plugin-babel');
 const watch = require('gulp-watch');
+const path = require('path');
+
+const year = new Date().getFullYear();
+const pkg = require(path.resolve(__dirname, 'package.json'));
+const banner =
+`/*!
+  * CrumbsJS v${pkg.version}
+  * ${pkg.homepage}
+  * Copyright 2018 - ${year}, ${pkg.author}
+  * Licensed under MIT (https://github.com/nirtz89/crumbsjs/blob/master/LICENSE)
+  */`;
 
 gulp.task('compress', function() {
   gulp.src(['src/*.js'])
+    .pipe(gulpRollup({
+      input: './src/crumbs.js',
+      output: {
+        banner,
+        file: './dist/crumbs.js',
+        format: 'umd',
+        name: 'crumbs'
+      },
+      plugins: [
+        babel({
+          exclude: 'node_modules/**'
+        })
+      ]
+    }))
     .pipe(minify())
     .pipe(gulp.dest('dist'))
 });
